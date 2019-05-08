cce732baa3b56bc773d5955654c36d989f3750bb
mazdak <nazemi.works@gmail.com>
2019-05-08 02:36:24 -0700

    heroku ready port and fetch args refined

--- public/js/app.js
+++ public/js/app.js
@@ -17,20 +17,20 @@ const messageTwo = document.querySelector('#message-2');
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const location = searchEl.value;
-  messageOne.textContent = '           ';
-  messageTwo.textContent = '           ';
+  messageOne.textContent = ' ';
+  messageTwo.textContent = '';
 
-  fetch(`http://localhost:3000/weather?address=${location}`)
+  fetch(`/weather?address=${location}`)
     .then((response) => {
       response.json()
         .then((data) => {
           if (data.error) {
-            console.log(data.error);
+            // console.log(data.error);
             messageOne.textContent = data.error;
           } else {
-            console.log(data.location);
+            // console.log(data.location);
             messageOne.textContent = data.location;
-            console.log(data.forecast);
+            // console.log(data.forecast);
             messageTwo.textContent = data.forecast;
           }
         })
--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ const forecast = require('./utils/forecast');
 //app.com
 //app.com/help
 const app = express();
+const port = process.env.PORT || 3000;
+
 
 
 // cl __dirname and __filename
@@ -137,6 +139,6 @@ app.get('*', (req, res) => {
 
 
 //server runner
-app.listen(3000, () => {
-  console.log('server is up on port 3000!...');
+app.listen(port, () => {
+  console.log(`server is up on port ${port}!...`);
 })
